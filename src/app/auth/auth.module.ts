1ebb1653eb56f4293e76239b7070f75fc185fd9f
sincherer <ersincher@gmail.com>
2025-06-07 17:26:52 +0800

    update sign up

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     CommonModule,
@@ -17,7 +19,8 @@ import { AuthService } from './auth.service';
     AuthService
   ],
   exports: [
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ]
 })
 export class AuthModule { }
